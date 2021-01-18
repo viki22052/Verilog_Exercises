39b48f61643c504820ae8916b97f17f93ae807b3
viki22052 <35682409+viki22052@users.noreply.github.com>
2021-01-18 11:51:02 +0200

    Fixed Counter

--- Memory_dr/counter.v
+++ Memory_dr/counter.v
@@ -3,21 +3,34 @@ module counter(vin, clk, dataout, vout);
 	input vin, clk;
 	output reg [7:0] dataout;
 	output reg vout;
-	reg br;
+	integer br;
+	
+	initial begin
+	
+		dataout = 0;
+		br = 0;
+		
+	end
 	
 	always @(posedge vin) begin
-		vout = 1'b0;
+		
 		
 		if(clk) begin
-			
+		
+			vout = 1'b0;
 			dataout = dataout + 1;
 			br = br + 1;
-			end
+			
+		end
 			
 		if(br == 4) begin
+	
 			vout = 1'b1;
 			br = 0;
-			end
+			
+		end
 			
 	end
+	
+	
 endmodule
